0d2cd7983b209bac228fedb2fac2813a3e5d263b
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-20 14:22:54 +0200

    feat: dynamic sidenav

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,4 +6,24 @@ export const routes: Routes = [
         title: 'Home',
         loadComponent: () => import('@pages/home/home.component').then(c => c.HomeComponent)
     },
+    {
+        path: 'programs',
+        title: 'Programs',
+        loadComponent: () => import('@pages/programs/programs.component').then(c => c.ProgramsComponent)
+    },
+    {
+        path: 'groceries',
+        title: 'Groceries',
+        loadComponent: () => import('@pages/groceries/groceries.component').then(c => c.GroceriesComponent)
+    },
+    {
+        path: 'training',
+        title: 'Training',
+        loadComponent: () => import('@pages/training/training.component').then(c => c.TrainingComponent)
+    },
+    {
+        path: 'progress',
+        title: 'Progress',
+        loadComponent: () => import('@pages/progress/progress.component').then(c => c.ProgressComponent)
+    },
 ];
--- frontend/src/app/components/sidenav/sidenav.component.ts
+++ frontend/src/app/components/sidenav/sidenav.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { routes } from '@app/app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [SidebarModule, ButtonModule, RouterLink, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  routes = routes;
   sidebarVisible = false;
 }
