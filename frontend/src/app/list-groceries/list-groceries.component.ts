fd57432cf7e8fbb730f48f6ec3142cf545a40986
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-20 11:19:31 +0200

    feat: list-groceries component

---
+++ frontend/src/app/list-groceries/list-groceries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-groceries',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-groceries.component.html',
+  styleUrl: './list-groceries.component.css'
+})
+export class ListGroceriesComponent {
+
+}
