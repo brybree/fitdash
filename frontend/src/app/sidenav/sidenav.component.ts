cc8603424cc17e8eb76195b74ffb2df13592bc8f
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-20 12:11:02 +0200

    feat: working sidebar

--- frontend/src/app/sidenav/sidenav.component.ts
+++ frontend/src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [SidebarModule, ButtonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
-
+  sidebarVisible = false;
 }
