4731f630e35b1a66bb554977bbbbb6f659293bd9
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-21 17:38:19 +0200

    wip: programs page

--- frontend/src/app/pages/programs/programs.component.ts
+++ frontend/src/app/pages/programs/programs.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-programs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './programs.component.html',
   styleUrl: './programs.component.css'
 })
 export class ProgramsComponent {
 
+  fake_data = [
+    "Upper Body 1",
+    "Upper Body 2",  
+    "Lower Body 1", 
+    "Lower Body 2" ,
+    "Full Body 1",
+    "Full Body 2",
+  ]
 }
