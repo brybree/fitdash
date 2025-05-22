1d97660bb398119179250efed29d710977b953d7
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-22 14:54:36 +0200

    feat: few colors to rule them all

---
+++ frontend/src/app/pages/color-test/color-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './color-test.component.html',
+  styleUrl: './color-test.component.css'
+})
+export class ColorTestComponent {
+
+}
