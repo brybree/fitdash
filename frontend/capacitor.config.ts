a9a3331f121929494e12f801a90ccb08b03af9c9
bonnty <43603875+brybree@users.noreply.github.com>
2025-05-30 19:45:26 +0200

    fix: project name, still need it in cli arg tho

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fitdash',
+  appName: 'frontend',
   webDir: 'www'
 };
 
